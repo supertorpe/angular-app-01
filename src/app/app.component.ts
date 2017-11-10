a36a2d81a15ef9d10e6b0fcae511f51185b4ebc9
supertorpe <supertorpe@hotmail.com>
2017-11-10 15:13:48 +0100

    scss + bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = 'App rocks!';
+  }
 }
